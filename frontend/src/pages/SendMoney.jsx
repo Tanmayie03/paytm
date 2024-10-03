94b67dc6e9daf82bf26d6b65e4ceae1e8c640822
tanmayiepatil <120322328+Tanmayie03@users.noreply.github.com>
2024-10-04 01:04:06 +0530

    feat:All the functionalties added to the application

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -1,4 +1,4 @@
-import { useSearchParams } from "react-router-dom";
+import { Navigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 
@@ -6,6 +6,7 @@ export const SendMoney = () => {
   const [searchParams] = useSearchParams();
   const id = searchParams.get("id");
   const name = searchParams.get("name");
+
   const [amount, setAmount] = useState(0);
 
   return (
@@ -18,9 +19,11 @@ export const SendMoney = () => {
           <div className="p-6">
             <div className="flex items-center space-x-4">
               <div className="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
-                <span className="text-2xl text-white">{name}</span>
+                <span className="text-2xl text-white">
+                  {name[0].toUpperCase()}
+                </span>
               </div>
-              <h3 className="text-2xl font-semibold">{name}</h3>
+              <h3 className="text-2xl font-semibold capitalize">{name}</h3>
             </div>
             <div className="space-y-4">
               <div className="space-y-2">
@@ -29,10 +32,9 @@ export const SendMoney = () => {
                   htmlFor="amount">
                   Amount (in Rs)
                 </label>
+
                 <input
-                  onChange={(e) => {
-                    setAmount(e.target.value);
-                  }}
+                  onChange={(e) => setAmount(e.target.value)}
                   type="number"
                   className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
                   id="amount"
@@ -54,6 +56,7 @@ export const SendMoney = () => {
                       },
                     }
                   );
+                  Navigate("/dashboard");
                 }}
                 className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
                 Initiate Transfer
